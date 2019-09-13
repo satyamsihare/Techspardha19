d734a6f234088741b689d17f32651f6209345cc2
prathamzx <pratham.1999@yahoo.in>
2019-09-13 23:34:46 +0530

    Worked on Event Page

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState, useContext } from 'react';
 import Back from './Back';
+import Typist from 'react-typist';
 import Context from '../contextStore/Context';
 import Loading from './Loading';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Sound from 'react-sound';
 const EventList = props => {
+  const [bool,setState1]=useState("PLAYING");
   const category = props.match.params.category;
   const { dispatch } = useContext(Context);
   const [istate, setState] = useState({
     events: []
   });
-
   useEffect(() => {
     const getEvents = async () => {
       try {
@@ -35,25 +37,39 @@ const EventList = props => {
     };
     getEvents();
   }, []);
+  const pause=function pausemusic(){
+    console.log("Typing finished");
+    setState1("PAUSED");
+  }
   console.log(istate);
   if (istate.events.length <= 0) return <p>fetching events..</p>;
   return (
     <>
-      <Loading title={`${category}`} />
+    <Loading title={`${category}`} />
       <div className='c-container'>
         <Back history={props} />
         <h1>{`/${category}`}</h1>
+
         <div className='ievent-list'>
           <ul>
             {istate.events
               .filter(event => event.eventCategory === category)
               .map((event, index) => (
                 <Link key={index} to={`/events/${category}/${event.eventName}`}>
+                <Typist
+                  startDelay={50}
+                  avgTypingDelay={60}
+                  stdTypingDelay={300}
+                  cursor={{show:false, blink:true, element:'>', hideWhenDone:true }}
+                  onTypingDone={pause}
+                 >
                   <li className='cnt-item'>> {event.eventName}</li>
+                    </Typist>
                 </Link>
               ))}
           </ul>
         </div>
+
       </div>
     </>
   );
